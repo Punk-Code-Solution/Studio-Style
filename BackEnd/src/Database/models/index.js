14d582584fea17e10959707a9ee233cf5ed29117
Thiago de Freitas <48700732+ThiagoFreitasFreelancer@users.noreply.github.com>
2025-09-19 12:29:53 -0300

    Update index.js

--- BackEnd/src/Database/models/index.js
+++ BackEnd/src/Database/models/index.js
@@ -14,6 +14,7 @@ const dbConfig = {
   database: process.env.DB_DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   dialect: 'postgres',
 };
 
