45fea0edcb5d237f2e584fb95ca2cedb1e5aa7cf
Thiago Freitas <tfgoncalvesfreelancer@gmail.com>
2025-12-13 17:30:14 -0300

    Correções em whatsapp

--- FrontEnd/src/main.server.ts
+++ FrontEnd/src/main.server.ts
@@ -1,7 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-const bootstrap = (context: any) => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
+export default async function bootstrap() {
+  return bootstrapApplication(AppComponent, config);
+}
